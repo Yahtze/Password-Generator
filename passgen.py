4c7f9958e4a3cb0e31598400cf8e1b1759408d81
Yahtze <65290508+Yahtze@users.noreply.github.com>
2023-01-29 11:33:33 +0530

    Initial Logic

--- passgen.py
+++ passgen.py
@@ -1,4 +1,18 @@
 import random
 
+def shufflePass(string):
+    tempList = list(string)
+    random.shuffle(tempList)
+    string = ''.join(tempList)
+    print(string)
 
-upperCaseLetter = chr(random.randint(65, 90))
+upperCaseLetter1 = chr(random.randint(65, 90))
+upperCaseLetter2 = chr(random.randint(65, 90))
+numberOne = chr(random.randint(0, 9))
+numberTwo = chr(random.randint(0, 9))
+lowerCaseLetter1 = chr(random.randint(97, 122))
+lowerCaseLetter2 = chr(random.randint(97, 122))
+specialSign1 = chr(random.randint(33, 47))
+specialSign2 = chr(random.randint(33, 47))
+password = upperCaseLetter1 + upperCaseLetter2 + specialSign1 + specialSign2 + numberOne + numberTwo + lowerCaseLetter1 + lowerCaseLetter2
+shufflePass(password)
